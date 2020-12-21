060dcc241704e81684f1eabab627f89021d6988e
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-12-21 12:54:51 +0000

    Renamed package

---
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/Application.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.em.hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableSwagger2
+@EnableScheduling
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- src/test/java/uk/gov/hmcts/reform/em/hrs/DemoUnitTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/DemoUnitTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.rsecheck;
+package uk.gov.hmcts.reform.em.hrs;
 
 import org.junit.jupiter.api.Test;
 
