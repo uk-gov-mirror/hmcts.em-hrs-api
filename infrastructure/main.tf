c5aa8b6affb631e14ae7f8f133401ac617d15fc5
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-12-25 08:48:17 +0000

    Added in keyvault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,26 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = "${var.location}"
+
+  tags = "${var.common_tags}"
+}
+
+module "key-vault" {
+  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product                    = var.product
+  env                        = var.env
+  tenant_id                  = var.tenant_id
+  object_id                  = var.jenkins_AAD_objectId
+  resource_group_name        = azurerm_resource_group.rg.name
+  common_tags                = var.common_tags
+  managed_identity_object_id = ["${data.azurerm_user_assigned_identity.rpa-shared-identity.principal_id}"]
+}
+
+data "azurerm_user_assigned_identity" "rpa-shared-identity" {
+  name                = "rpa-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
