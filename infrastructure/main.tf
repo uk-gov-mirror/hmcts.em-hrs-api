8e0148ecb75b2fc379f5155699591529bf81f67a
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-12-25 22:33:33 +0000

    Updated db details

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -42,8 +42,32 @@ module "db" {
   subscription       = var.subscription
 }
 
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.db.user_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name         = "${local.app_full_name}-POSTGRES-PASS"
+  name         = "${var.component}-POSTGRES-PASS"
   value        = module.db.postgresql_password
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.db.host_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = "5432"
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.db.postgresql_database
   key_vault_id = module.key-vault.key_vault_id
 }
