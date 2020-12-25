f62fe8514af481aafae55b0826b1479948896621
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-12-25 15:13:17 +0000

    Added in product_group_object_id

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  app_full_name = "${var.product}-${var.component}"
+
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = "${var.location}"
@@ -11,16 +16,17 @@ resource "azurerm_resource_group" "rg" {
 
 module "key-vault" {
   source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                    = var.product
+  product                    = local.app_full_name
   env                        = var.env
   tenant_id                  = var.tenant_id
   object_id                  = var.jenkins_AAD_objectId
   resource_group_name        = azurerm_resource_group.rg.name
+  product_group_object_id    = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
   common_tags                = var.common_tags
-  managed_identity_object_id = ["${data.azurerm_user_assigned_identity.rpa-shared-identity.principal_id}"]
+  managed_identity_object_id = ["${data.azurerm_user_assigned_identity.em-shared-identity.principal_id}"]
 }
 
-data "azurerm_user_assigned_identity" "rpa-shared-identity" {
+data "azurerm_user_assigned_identity" "em-shared-identity" {
   name                = "rpa-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
 }
