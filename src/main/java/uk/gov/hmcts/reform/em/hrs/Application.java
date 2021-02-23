b79a72d0499f2da0b92cb52118acb74ba9117189
Semenu Aggor <semenu.aggor@HMCTS.NET>
2021-02-23 09:39:59 +0000

    Remove redundant swagger2 annotation

--- src/main/java/uk/gov/hmcts/reform/em/hrs/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/Application.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class excluded as using repository finder pattern?
-// Without these exclusions it compains about various dependencies that are there
+// Without these exclusions it complains about various dependencies that are there
 @SpringBootApplication
 //(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@EnableSwagger2
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
