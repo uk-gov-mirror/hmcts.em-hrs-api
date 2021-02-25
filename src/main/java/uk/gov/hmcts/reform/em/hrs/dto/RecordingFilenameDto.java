3b19ae828cad4515f8557b811b176fe56532933e
Semenu Aggor <semenu.aggor@HMCTS.NET>
2021-02-25 16:46:46 +0000

    EM-3389 Use kebab case in json and return filenames as a set

--- src/main/java/uk/gov/hmcts/reform/em/hrs/dto/RecordingFilenameDto.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/dto/RecordingFilenameDto.java
@@ -3,11 +3,11 @@ package uk.gov.hmcts.reform.em.hrs.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @AllArgsConstructor
 public class RecordingFilenameDto {
     private final String folderName;
-    private final List<String> filenames;
+    private final Set<String> filenames;
 }
