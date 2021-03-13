cfb678ee1bfae3ae56153f5e953c9eb953e54ad7
taleb <taleb.benouaer@hmcts.net>
2021-03-13 12:19:31 +0000

    tidy up api interactions

---
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/tokens/S2sHelper.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.reform.em.hrs.service.tokens;
+
+public class S2sHelper {
+    public String getS2sToken() {
+        return "S2S TOKEN";// CALL auth-provider to get actual S2S token
+    }
+}
