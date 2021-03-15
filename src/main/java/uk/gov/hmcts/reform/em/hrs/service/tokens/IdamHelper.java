836b3d992386b6d4382ec187eb8ef9c6460eaf6b
taleb <taleb.benouaer@hmcts.net>
2021-03-15 20:33:01 +0000

    setup auth tokens

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/tokens/IdamHelper.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/tokens/IdamHelper.java
@@ -1,14 +1,30 @@
 package uk.gov.hmcts.reform.em.hrs.service.tokens;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+
+@Service
 public class IdamHelper {
 
-    private String SYSTEM_USER_NAME;
+    private final IdamClient idamClient;
 
-    public String getUserToken() {
-        return "userToken";//TODO - Call IDAM/getUserDetails to get userToken
+    private final String hrsSystemUserName;
+    private final String hrsSystemUserPassword;
+
+    public IdamHelper(IdamClient idamClient,
+                      @Value("auth.idam.system-user.username") String username,
+                      @Value("auth.idam.system-user.password") String password) {
+        this.idamClient = idamClient;
+        this.hrsSystemUserName = username;
+        this.hrsSystemUserPassword = password;
     }
 
-    public String getUserId() {
-        return "USER_ID";//TODO -  GET ACTUAL SYSTEM USER ID FROM IDAM
+    public String getUserToken() {
+        return idamClient.getAccessToken(hrsSystemUserName, hrsSystemUserPassword);
+    }
+
+    public String getUserId(String userAuthorization) {
+        return idamClient.getUserDetails(userAuthorization).getId();
     }
 }
--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/tokens/S2sHelper.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.em.hrs.service.tokens;
-
-public class S2sHelper {
-    public String getS2sToken() {
-        return "S2S TOKEN";// CALL auth-provider to get actual S2S token
-    }
-}
