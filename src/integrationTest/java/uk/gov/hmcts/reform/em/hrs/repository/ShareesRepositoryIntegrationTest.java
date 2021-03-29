b58c03def3d5de1df68eb88a0a22c0eed7bca294
Lara <laratollington@gmail.com>
2021-03-29 16:45:11 +0100

    EM-3405 Test auditing LocalDateTime fields are setup correctly

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/repository/ShareesRepositoryIntegrationTest.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.reform.em.hrs.repository;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.em.hrs.domain.HearingRecordingSharee;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.em.hrs.componenttests.TestUtil.HEARING_RECORDING;
+
+class ShareesRepositoryIntegrationTest extends AbstractRepositoryIntegrationTest {
+    @Inject
+    private ShareesRepository underTest;
+
+    private static final String EMAIL_ADDRESS = "test@testEmail.com";
+
+    @Test
+    void testShouldSaveSharee() {
+        final HearingRecordingSharee hearingRecordingSharee = HearingRecordingSharee.builder()
+            .hearingRecording(HEARING_RECORDING)
+            .shareeEmail(EMAIL_ADDRESS).build();
+
+        final HearingRecordingSharee savedSharee = underTest.save(hearingRecordingSharee);
+
+        assertThat(savedSharee).satisfies(x -> {
+            assertThat(x.getShareeEmail()).isEqualTo(EMAIL_ADDRESS);
+            assertThat(x.getHearingRecording()).isNotNull();
+        });
+    }
+
+    @Test
+    void testAuditingFieldsArePopulated() {
+        final LocalDateTime preTest = LocalDateTime.now(Clock.systemDefaultZone());
+        final HearingRecordingSharee hearingRecordingSharee = HearingRecordingSharee.builder()
+            .hearingRecording(HEARING_RECORDING)
+            .shareeEmail(EMAIL_ADDRESS).build();
+
+        final HearingRecordingSharee savedSharee = underTest.save(hearingRecordingSharee);
+
+        final LocalDateTime postTest = LocalDateTime.now(Clock.systemDefaultZone());
+        assertThat(savedSharee).satisfies(x -> {
+            assertThat(x.getSharedOn()).isBetween(preTest, postTest);
+            //assertThat(x.getSharedByRef()).isNotNull();
+        });
+    }
+}
