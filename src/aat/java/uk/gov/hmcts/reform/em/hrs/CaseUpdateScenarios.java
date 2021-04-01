e942eb554535fc34d6b07cacdeb3f3371f6bd856
taleb <20416858+benouaer@users.noreply.github.com>
2021-04-01 17:39:30 +0100

    add missing import

--- src/aat/java/uk/gov/hmcts/reform/em/hrs/CaseUpdateScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/hrs/CaseUpdateScenarios.java
@@ -7,6 +7,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
