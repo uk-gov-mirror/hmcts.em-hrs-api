1f8526a767aa49ff766382713a09a6428d598986
shehreem <shehreem.haddad@solirius.com>
2021-04-06 14:39:45 +0100

    Update SetUtils.java

--- src/main/java/uk/gov/hmcts/reform/em/hrs/util/SetUtils.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/util/SetUtils.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.em.hrs.util;
 
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-import javax.validation.constraints.NotNull;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,14 +12,14 @@ import java.util.stream.Stream;
 @NoArgsConstructor()
 public final class SetUtils {
 
-    public static <T> Set<T> intersect(@NotNull final Set<T> setA, @NotNull final Set<T> setB) {
+    public static <T> Set<T> intersect(@NonNull final Set<T> setA, @NonNull final Set<T> setB) {
         Set<T> a = new HashSet<>(setA);
         Set<T> b = new HashSet<>(setB);
 
         return a.retainAll(b) ? Collections.unmodifiableSet(a) : setA;
     }
 
-    public static <T> Set<T> union(@NotNull final Set<T> setA, @NotNull final Set<T> setB) {
+    public static <T> Set<T> union(@NonNull final Set<T> setA, @NonNull final Set<T> setB) {
         return Stream.concat(setA.stream(), setB.stream())
             .collect(Collectors.toSet());
     }
