8c4ce32979142c96ab7f27f344ea6342e6c2f5f3
Pettedson John <pettedson.john@hmcts.net>
2021-04-08 09:33:01 +0100

    Interim Commit as we have to rebase with master....

--- src/aat/java/uk/gov/hmcts/reform/em/hrs/GetFileNamesScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/hrs/GetFileNamesScenarios.java
@@ -1,25 +1,21 @@
 package uk.gov.hmcts.reform.em.hrs;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.hmcts.reform.em.EmTestConfig;
-import uk.gov.hmcts.reform.em.hrs.repository.JobInProgressRepository;
 import uk.gov.hmcts.reform.em.hrs.testutil.AuthTokenGeneratorConfiguration;
-import uk.gov.hmcts.reform.em.hrs.testutil.CcdAuthTokenGeneratorConfiguration;
-import uk.gov.hmcts.reform.em.test.retry.RetryRule;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
-import javax.inject.Inject;
 
 @SpringBootTest(classes = {EmTestConfig.class, AuthTokenGeneratorConfiguration.class})
 @TestPropertySource(value = "classpath:application.yml")
 @RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "/db/populate_db.sql")
 public class GetFileNamesScenarios {
 
     /*@Inject
