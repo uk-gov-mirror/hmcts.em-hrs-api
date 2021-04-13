2d2df27eaf6f26b7cc5cbeaaa72a834a22742aa8
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-04-13 14:53:59 +0100

    Removed Test DB Annotation

--- src/aat/java/uk/gov/hmcts/reform/em/hrs/AbstractBaseScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/hrs/AbstractBaseScenarios.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.em.hrs;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.hmcts.reform.em.EmTestConfig;
@@ -16,6 +15,5 @@ import uk.gov.hmcts.reform.em.hrs.testutil.ExtendedCcdHelper;
     properties = {"classpath:application.yml"}
 )
 @RunWith(SpringJUnit4ClassRunner.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AbstractBaseScenarios {
 }
