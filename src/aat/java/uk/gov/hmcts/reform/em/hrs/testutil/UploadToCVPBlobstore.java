d041806685a3f39c9ae42202c02295d1e7a91aa6
shehreem <shehreem.haddad@solirius.com>
2021-04-21 17:47:24 +0100

    trying to access blobstore through code to upload a file

---
+++ src/aat/java/uk/gov/hmcts/reform/em/hrs/testutil/UploadToCVPBlobstore.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.em.hrs.testutil;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+
+import java.io.File;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class UploadToCVPBlobstore {
+ //   https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-java?tabs=powershell
+
+
+    public static void uploadBlob() throws IOException {
+
+        // Create a BlobServiceClient object which will be used to create a container client
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1").buildClient();
+
+        //Create a unique name for the container
+        String containerName = "cvptestcontainer" + java.util.UUID.randomUUID();
+
+        // Create the container and return a container client object
+        BlobContainerClient containerClient = blobServiceClient.createBlobContainer(containerName);
+
+
+    // Create a local file in the ./data/ directory for uploading and downloading
+        String localPath = "./audiostream01";
+        String fileName = "quickstart" + java.util.UUID.randomUUID() + ".txt";
+        File localFile = new File(localPath + fileName);
+
+// Write text to the file
+        FileWriter writer = new FileWriter(localPath + fileName, true);
+        writer.write("Hello, World!");
+        writer.close();
+
+// Get a reference to a blob
+        BlobClient blobClient = containerClient.getBlobClient(fileName);
+
+        System.out.println("\nUploading to Blob storage as blob:\n\t" + blobClient.getBlobUrl());
+
+// Upload the blob
+        blobClient.uploadFromFile(localPath + fileName);
+    }
+
+    public static void main(String[] args) throws IOException {
+uploadBlob();
+    }
+}
