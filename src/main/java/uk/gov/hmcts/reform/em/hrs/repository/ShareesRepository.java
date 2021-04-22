65a200a422d4625e4950cb12406b8e2315cd36e5
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-04-22 16:04:07 +0100

    Resolved Sonar issue

--- src/main/java/uk/gov/hmcts/reform/em/hrs/repository/ShareesRepository.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/repository/ShareesRepository.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.em.hrs.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.reform.em.hrs.domain.HearingRecording;
 import uk.gov.hmcts.reform.em.hrs.domain.HearingRecordingSharee;
 
 import java.util.List;
