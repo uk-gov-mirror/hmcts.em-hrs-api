63f526fe53bb7535770aa8769cda582fc72ade7b
Paul Crimes <paul.crimes@solirius.com>
2021-04-30 13:11:42 +0100

    flesh out test conditions for cvp connection resolver

--- src/test/java/uk/gov/hmcts/reform/em/hrs/util/CvpConnectionResolverTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/util/CvpConnectionResolverTest.java
@@ -1,15 +1,21 @@
 package uk.gov.hmcts.reform.em.hrs.util;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
+import static uk.gov.hmcts.reform.em.hrs.util.CvpConnectionResolver.isACvpEndpointUrl;
 
 public class CvpConnectionResolverTest {
     @Test
     public void testIsACvpEndpointUrl() {
-        assertFalse(CvpConnectionResolver.isACvpEndpointUrl("https://example.org/example"));
-        assertTrue(CvpConnectionResolver.isACvpEndpointUrl("cvprecordings"));
+        assertFalse(isACvpEndpointUrl(
+            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;" +
+                "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;" +
+                "BlobEndpoint=http://localhost:10000/devstoreaccount1"));
+        assertTrue(isACvpEndpointUrl("https://cvprecordingssboxsa.blob.core.windows.net/"));
+        assertTrue(isACvpEndpointUrl("https://cvprecordingsstgsa.blob.core.windows.net/"));
+        assertTrue(isACvpEndpointUrl("https://cvprecordingsprodsa.blob.core.windows.net/"));
     }
 }
 
