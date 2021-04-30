7e28f114ecd1c58e62d8cd05daad996621cca3c3
Paul Crimes <paul.crimes@solirius.com>
2021-04-30 17:58:33 +0100

    improved coverage on cvs connection string resolver

--- src/test/java/uk/gov/hmcts/reform/em/hrs/util/CvpConnectionResolverTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/util/CvpConnectionResolverTest.java
@@ -10,9 +10,15 @@ public class CvpConnectionResolverTest {
     @Test
     public void testIsACvpEndpointUrl() {
         assertFalse(isACvpEndpointUrl(
-            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;" +
-                "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;" +
-                "BlobEndpoint=http://localhost:10000/devstoreaccount1"));
+            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
+                + "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
+                + "BlobEndpoint=http://localhost:10000/devstoreaccount1"));
+        assertFalse(isACvpEndpointUrl(
+            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
+                + "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
+                + "BlobEndpoint=https://cvprecordingssboxsa.blob.core.windows.net/"));
+        assertFalse(isACvpEndpointUrl("https://hrsendpoint.blob.core.windows.net/"));
+
         assertTrue(isACvpEndpointUrl("https://cvprecordingssboxsa.blob.core.windows.net/"));
         assertTrue(isACvpEndpointUrl("https://cvprecordingsstgsa.blob.core.windows.net/"));
         assertTrue(isACvpEndpointUrl("https://cvprecordingsprodsa.blob.core.windows.net/"));
