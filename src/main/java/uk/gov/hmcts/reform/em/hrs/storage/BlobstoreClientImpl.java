042837c2cd0754188e31fc64c402db9431afceb4
Paul Crimes <paul.crimes@solirius.com>
2021-05-28 09:48:34 +0100

    disable partial response

--- src/main/java/uk/gov/hmcts/reform/em/hrs/storage/BlobstoreClientImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/storage/BlobstoreClientImpl.java
@@ -3,21 +3,17 @@ package uk.gov.hmcts.reform.em.hrs.storage;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.models.BlobProperties;
 import com.azure.storage.blob.models.BlobRange;
-import com.azure.storage.blob.models.DownloadRetryOptions;
 import com.azure.storage.blob.specialized.BlockBlobClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.em.hrs.exception.InvalidRangeRequestException;
 
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -85,17 +81,17 @@ public class BlobstoreClientImpl implements BlobstoreClient {
     private void loadFullBlob(String filename, BlockBlobClient blobClient, HttpServletResponse response)
         throws IOException {
 
-        LOGGER.info(
-            "Not downloading blob, responding with Error",
-            filename
-        );//this means ONLY partial requests are honoured
-
-        response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE);
-
-        //        LOGGER.info("Loading Full Blob{}", filename);
-        //        //blobClient.download(response.getOutputStream());
+        //        LOGGER.info(
+        //            "Not downloading blob, responding with Error",
+        //            filename
+        //        );//this means ONLY partial requests are honoured
         //
-        //        LOGGER.info("Reading Blob from Azure Blob Storage: OK {}", filename);
+        //        response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE);
+        //
+        LOGGER.info("Loading Full Blob{}", filename);
+        blobClient.download(response.getOutputStream());
+
+        LOGGER.info("Reading Blob from Azure Blob Storage: OK {}", filename);
     }
 
 
@@ -108,47 +104,54 @@ public class BlobstoreClientImpl implements BlobstoreClient {
         String rangeHeader = request.getHeader("range");
         LOGGER.info("Range requested: {}", rangeHeader);
 
-        Long length = fileSize;
 
-        String patternString = "^bytes=\\d*-\\d*";
-
-        Pattern pattern = Pattern.compile(patternString);
-
-        Matcher matcher = pattern.matcher(rangeHeader);
-
-        if (!matcher.matches()) {
-            throw new InvalidRangeRequestException(response, length);
-        }
-
-        response.setBufferSize(DEFAULT_BUFFER_SIZE);
-
-        // Range headers can request a multipart range but this is not to be supported yet
-        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
-        // Take only first requested range and process it
-        String byteRange = rangeHeader.substring(6).split(",")[0].trim();
-
-        BlobRange blobRange = setContentRangeHeadersAndGenerateBlobRange(byteRange, length, response);
+        LOGGER.info("Not Processing range, sending error instead:");
+        response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE);
 
 
-        if (blobRange.getOffset() == 0 && blobRange.getCount() > length) {
-            LOGGER.info("Offset =0 and b count > length {}", filename);
-            loadFullBlob(filename, blobClient, response);
-            return;
-        }
+        //        Long length = fileSize;
+        //
+        //        String patternString = "^bytes=\\d*-\\d*";
+        //
+        //        Pattern pattern = Pattern.compile(patternString);
+        //
+        //        Matcher matcher = pattern.matcher(rangeHeader);
+        //
+        //        if (!matcher.matches()) {
+        //            throw new InvalidRangeRequestException(response, length);
+        //        }
+        //
+        //        response.setBufferSize(DEFAULT_BUFFER_SIZE);
+        //
+        //        // Range headers can request a multipart range but this is not to be supported yet
+        //        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
+        //        // Take only first requested range and process it
+        //        String byteRange = rangeHeader.substring(6).split(",")[0].trim();
+        //
+        //        BlobRange blobRange = setContentRangeHeadersAndGenerateBlobRange(byteRange, length, response);
+        //
+        //
+        //        if (blobRange.getOffset() == 0 && blobRange.getCount() > length) {
+        //            LOGGER.info("Offset =0 and b count > length {}", filename);
+        //            loadFullBlob(filename, blobClient, response);
+        //            return;
+        //        }
+        //
+        //        response.setStatus(HttpStatus.PARTIAL_CONTENT.value());
+        //
 
-        response.setStatus(HttpStatus.PARTIAL_CONTENT.value());
 
-        LOGGER.info("Processing blob range: {}", blobRange.toString());
-        blockBlobClient(filename.toString())
-            .downloadWithResponse(
-                response.getOutputStream(),
-                blobRange,
-                new DownloadRetryOptions().setMaxRetryRequests(5),
-                null,
-                false,
-                null,
-                null
-            );
+        //        LOGGER.info("Processing blob range: {}", blobRange.toString());
+        //        blockBlobClient(filename.toString())
+        //            .downloadWithResponse(
+        //                response.getOutputStream(),
+        //                blobRange,
+        //                new DownloadRetryOptions().setMaxRetryRequests(5),
+        //                null,
+        //                false,
+        //                null,
+        //                null
+        //            );
     }
 
     private BlobRange setContentRangeHeadersAndGenerateBlobRange(String part, Long length,
