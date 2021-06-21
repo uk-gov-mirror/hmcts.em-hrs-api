4cdbed53f86186395f1d7a87d0b3fef9f1c602dd
shehreem <shehreem.haddad@solirius.com>
2021-06-21 16:08:22 +0100

    using 10 tries of 30 seconds each

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/HearingRecordingSegmentScenarios.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/HearingRecordingSegmentScenarios.java
@@ -43,10 +43,10 @@ public class HearingRecordingSegmentScenarios extends BaseTest {
 
         int count = 0;
         while (testUtil.checkIfUploaded(FOLDER) <= 0) {
-            TimeUnit.SECONDS.sleep(60);
+            TimeUnit.SECONDS.sleep(30);
             count++;
 
-            if (count > 5)
+            if (count > 10)
             {
                 break;
             }
--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/ShareHearingRecordingScenarios.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/ShareHearingRecordingScenarios.java
@@ -36,10 +36,10 @@ public class ShareHearingRecordingScenarios extends BaseTest {
 
         int count = 0;
         while (testUtil.checkIfUploaded(FOLDER) <= 0) {
-            TimeUnit.SECONDS.sleep(60);
+            TimeUnit.SECONDS.sleep(30);
             count++;
 
-            if (count > 5)
+            if (count > 10)
             {
                 break;
             }
