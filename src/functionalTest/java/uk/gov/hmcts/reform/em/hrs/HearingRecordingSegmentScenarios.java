7985b18f3c13251bff2d35635a938649063a788d
shehreem <shehreem.haddad@solirius.com>
2021-06-21 11:54:24 +0100

    changing the 30 second break to 60 seconds

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/HearingRecordingSegmentScenarios.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/HearingRecordingSegmentScenarios.java
@@ -41,7 +41,7 @@ public class HearingRecordingSegmentScenarios extends BaseTest {
             .log().all()
             .statusCode(202);
 
-        TimeUnit.SECONDS.sleep(30);
+        TimeUnit.SECONDS.sleep(60);
 
         getFilenames(FOLDER)
             .assertThat().log().all()
