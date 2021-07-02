b7b7759dead939bdf90fbd1f27eac504c767218c
Paul Crimes <paul.crimes@solirius.com>
2021-07-02 13:31:52 +0100

    add delay and 2nd retry for getting MI token on app start up

--- src/main/java/uk/gov/hmcts/reform/em/hrs/listener/CheckEndPointsApplicationReadyListener.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/listener/CheckEndPointsApplicationReadyListener.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.em.hrs.storage.HearingRecordingStorage;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class CheckEndPointsApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
     private static final Logger LOGGER = LoggerFactory.getLogger(CheckEndPointsApplicationReadyListener.class);
@@ -21,6 +23,8 @@ public class CheckEndPointsApplicationReadyListener implements ApplicationListen
         LOGGER.info("Application is Ready!");
 
         try {
+            LOGGER.info("Sleeping 10 secs to allow token gen:");
+            TimeUnit.SECONDS.sleep(10);
             LOGGER.info("StorageReport:");
             String report = hearingRecordingStorage.getStorageReport();
             LOGGER.info(report);
@@ -28,6 +32,19 @@ public class CheckEndPointsApplicationReadyListener implements ApplicationListen
         catch (Exception e) {
             LOGGER.error("Unable to verify storage connectivity: {}",e.getMessage());
         }
+
+        try {
+            LOGGER.info("Retrying as a temp debug check, Sleeping another 10 secs to allow token gen:");
+            TimeUnit.SECONDS.sleep(10);
+            LOGGER.info("StorageReport:");
+            String report = hearingRecordingStorage.getStorageReport();
+            LOGGER.info(report);
+        }
+        catch (Exception e) {
+            LOGGER.error("Unable to verify 2nd storage connectivity: {}",e.getMessage());
+        }
+
+
 
     }
 
