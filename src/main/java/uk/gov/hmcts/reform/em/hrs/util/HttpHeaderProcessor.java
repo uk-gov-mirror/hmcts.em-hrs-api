4ffebceadddf97d6fee3c99bbeb07f85c1261126
Paul Crimes <paul.crimes@solirius.com>
2021-07-07 11:38:38 +0100

    fix up checkstyle warnings

--- src/main/java/uk/gov/hmcts/reform/em/hrs/util/HttpHeaderProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/util/HttpHeaderProcessor.java
@@ -1,8 +1,13 @@
 package uk.gov.hmcts.reform.em.hrs.util;
 
+import lombok.NoArgsConstructor;
+
 import javax.servlet.http.HttpServletRequest;
 
+import static lombok.AccessLevel.PRIVATE;
+
 //non frontdoor environments (ie DEMO do not use front door, and may use TitleCase header names)
+@NoArgsConstructor(access = PRIVATE)
 public class HttpHeaderProcessor {
 
     //front door environments use lowercase header names
