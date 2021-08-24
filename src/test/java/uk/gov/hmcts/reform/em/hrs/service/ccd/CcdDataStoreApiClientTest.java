d58cc52c160d85f300c1786655e7153cfc58f5ea
Paul Crimes <paul.crimes@solirius.com>
2021-08-24 17:23:29 +0100

    test exception handling

--- src/test/java/uk/gov/hmcts/reform/em/hrs/service/ccd/CcdDataStoreApiClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/service/ccd/CcdDataStoreApiClientTest.java
@@ -13,13 +13,16 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.em.hrs.dto.HearingRecordingDto;
+import uk.gov.hmcts.reform.em.hrs.exception.CcdUploadException;
 import uk.gov.hmcts.reform.em.hrs.service.SecurityService;
 
 import java.util.Map;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 class CcdDataStoreApiClientTest {
@@ -52,7 +55,8 @@ class CcdDataStoreApiClientTest {
     void shouldCreateCase() {
         doReturn(Map.of("user", USER_TOKEN,
                         "userId", USER_ID,
-                        "service", SERVICE_TOKEN)).when(securityService).getTokens();
+                        "service", SERVICE_TOKEN
+        )).when(securityService).getTokens();
         StartEventResponse startEventResponse = StartEventResponse.builder().build();
         doReturn(startEventResponse).when(coreCaseDataApi)
             .startCase(USER_TOKEN, SERVICE_TOKEN, CASE_TYPE, CREATE_CASE);
@@ -62,7 +66,7 @@ class CcdDataStoreApiClientTest {
         CaseDataContent caseData = CaseDataContent.builder().data(data)
             .event(Event.builder().build()).build();
         doReturn(caseDetails).when(coreCaseDataApi).submitForCaseworker(
-            USER_TOKEN, SERVICE_TOKEN, USER_ID, JURISDICTION, CASE_TYPE, false,caseData
+            USER_TOKEN, SERVICE_TOKEN, USER_ID, JURISDICTION, CASE_TYPE, false, caseData
         );
 
         Long caseId = underTest.createCase(RECORDING_ID, HEARING_RECORDING_DTO);
@@ -74,7 +78,8 @@ class CcdDataStoreApiClientTest {
     void shouldUpdateCase() {
         doReturn(Map.of("user", USER_TOKEN,
                         "userId", USER_ID,
-                        "service", SERVICE_TOKEN)).when(securityService).getTokens();
+                        "service", SERVICE_TOKEN
+        )).when(securityService).getTokens();
         StartEventResponse startEventResponse = StartEventResponse.builder()
             .caseDetails(CaseDetails.builder().id(CASE_ID).build())
             .build();
@@ -96,4 +101,39 @@ class CcdDataStoreApiClientTest {
 
         assertEquals(123456789L, caseId);
     }
+
+
+    @Test
+    void willHandleExceptionGracefully() {
+        doReturn(Map.of("user", USER_TOKEN,
+                        "userId", USER_ID,
+                        "service", SERVICE_TOKEN
+        )).when(securityService).getTokens();
+        StartEventResponse startEventResponse = StartEventResponse.builder()
+            .caseDetails(CaseDetails.builder().id(CASE_ID).build())
+            .build();
+        doReturn(startEventResponse).when(coreCaseDataApi).startEvent(
+            USER_TOKEN, SERVICE_TOKEN, String.valueOf(CASE_ID), ADD_RECORDING_FILE
+        );
+        CaseDetails caseDetails = CaseDetails.builder().id(CASE_ID).build();
+        JsonNode data = JsonNodeFactory.instance.objectNode();
+        doReturn(data).when(caseDataContentCreator)
+            .createCaseUpdateData(startEventResponse.getCaseDetails().getData(), RECORDING_ID, HEARING_RECORDING_DTO);
+        CaseDataContent caseData = CaseDataContent.builder().data(data)
+            .event(Event.builder().build()).build();
+
+
+        doThrow(RuntimeException.class).when(coreCaseDataApi).submitEventForCaseWorker(
+            USER_TOKEN, SERVICE_TOKEN, USER_ID,
+            JURISDICTION, CASE_TYPE, String.valueOf(CASE_ID), false, caseData
+        );
+
+        assertThatExceptionOfType(CcdUploadException.class).isThrownBy(() -> underTest.updateCaseData(
+            CASE_ID,
+            RECORDING_ID,
+            HEARING_RECORDING_DTO
+        ));
+
+    }
+
 }
