f764d66a4e6f930bd67afe597d4d9323483c542f
Paul Crimes <paul.crimes@solirius.com>
2022-01-26 16:54:00 +0000

    fix bash script user setup - fix roles string for hrs system user

--- docker/dependencies/idam-create-hrs-system-user.sh
+++ docker/dependencies/idam-create-hrs-system-user.sh
@@ -9,7 +9,7 @@ IDAM_URI=$1
 USERNAME=$2
 PASSWORD=$3
 
-ROLES="[{"code":"caseworker"},{"code":"caseworker-hrs"},{"code":"caseworker-hrs-searcher"}]"
+ROLES='[{"code":"caseworker"},{"code":"caseworker-hrs"},{"code":"caseworker-hrs-searcher"},{"code":"ccd-import"}]'
 
 echo "Creating User:"
 echo "IDAM_URI: $IDAM_URI"
@@ -21,7 +21,7 @@ echo "ROLES: $ROLES"
 
 DATA='{"email":"'${USERNAME}'", "password":"'${PASSWORD}'", "surname":"system" , "forename":"user", "roles":'${ROLES}'}'
 
-#echo "JSON DATA: $DATA"
+echo "JSON DATA: $DATA"
 
 curl -XPOST  "${IDAM_URI}/testing-support/accounts" \
      -H "accept: */*" \
