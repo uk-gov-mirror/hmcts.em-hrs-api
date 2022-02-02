9b55adda874b6a95034ed7434a52e0f11d0a8891
MUSTAFA <mustafa.dayican@hmcts.net>
2022-02-02 13:59:08 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -91,8 +91,7 @@ module "storage_account" {
   location                  = var.location
   account_kind              = "StorageV2"
   account_tier              = "Standard"
-  account_replication_type  = "
-  "
+  account_replication_type  = "ZRS"
   access_tier               = "Hot"
 
   enable_https_traffic_only = true
