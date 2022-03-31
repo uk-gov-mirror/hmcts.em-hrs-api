49a47ed9c55f1c91c62a3ad3e4fbbb525664cba0
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2022-03-31 20:18:45 +0100

    Updated azurerm & terraform version (#636)
    * Updated azurerm & terraform version
    
    * Fixed PR build failure

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.6.0"
+      version = "2.3.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0.2"
+      version = "~> 3.0.2"
     }
   }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -18,8 +18,8 @@ variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "common_tags" {
-  type = "map"
+variable common_tags {
+  type = map(string)
 }
 
 variable "postgresql_user" {
