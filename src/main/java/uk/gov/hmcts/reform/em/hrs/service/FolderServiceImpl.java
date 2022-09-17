c9033dd1d61363a3b2107d70072bd724272ab71a
MUSTAFA <mdayican@gmail.com>
2022-09-17 06:29:57 +0100

    EM-4601 add logs (#762)

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/FolderServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/FolderServiceImpl.java
@@ -64,16 +64,19 @@ public class FolderServiceImpl implements FolderService {
             .orElseThrow(() -> new DatabaseStorageException(FOLDER_MISSING_EXCEPTION_MSG));
     }
 
-
     private Set<String> getCompletedAndInProgressFiles(Folder folder) {
 
         Tuple2<FilesInDatabase, Set<String>> databaseRecords = getFilesetsFromDatabase(folder);
         FilesInDatabase filesInDatabase = databaseRecords.getT1();
-
+        
         Set<String> filesInBlobstore = hearingRecordingStorage.findByFolderName(folder.getName());
+        LOGGER.info("Files In Blob Store for folder={}, {}", folder.getName(), filesInDatabase);
 
         Set<String> completedFiles = filesInDatabase.intersect(filesInBlobstore);
+        LOGGER.info("Completed Files={}", completedFiles);
+
         Set<String> filesInProgress = databaseRecords.getT2();
+        LOGGER.info("FilesInProgress {}", filesInProgress);
 
         return SetUtils.union(completedFiles, filesInProgress);
     }
