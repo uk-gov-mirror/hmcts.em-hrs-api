e43a7d738941d1928b55d8e4426514bcf8a52e5c
mdayican <mustafa.dayican@hmcts.net>
2023-03-30 13:35:04 +0100

    add log

--- src/main/java/uk/gov/hmcts/reform/em/hrs/storage/HearingRecordingStorageImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/storage/HearingRecordingStorageImpl.java
@@ -98,6 +98,8 @@ public class HearingRecordingStorageImpl implements HearingRecordingStorage {
                     LOGGER.info("Generating and appending SAS token for copy for filename{}", filename);
                     String sasToken = generateReadSasForCvp(filename);
                     sourceUri = sourceUri + "?" + sasToken;
+                    LOGGER.info("Generated SasToken {}", sasToken);
+
                 }
 
                 LOGGER.info("SAS token created for filename{}", filename);
