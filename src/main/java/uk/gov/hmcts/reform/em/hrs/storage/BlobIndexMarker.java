4a23552f56f95a95a93f4b05b0a404b1a40ed64e
mdayican <mdayican@gmail.com>
2023-08-28 11:23:27 +0100

    ad logs

--- src/main/java/uk/gov/hmcts/reform/em/hrs/storage/BlobIndexMarker.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/storage/BlobIndexMarker.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.reform.em.hrs.storage;
 
 import com.azure.storage.blob.BlobContainerClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BlobIndexMarker {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlobIndexMarker.class);
+
     private static final String PROCESSED = "processed";
     private final BlobContainerClient blobContainerClient;
 
@@ -17,8 +22,10 @@ public class BlobIndexMarker {
     public boolean setProcessed(String blobName) {
         var blobClient = blobContainerClient.getBlobClient(blobName);
         var tags = blobClient.getTags();
+        LOGGER.info("blobName{} existing tags {}", blobName, tags);
         tags.put(PROCESSED, "true");
         blobClient.setTags(tags);
+        LOGGER.info("setTags done for {}", blobName);
         return true;
     }
 }
