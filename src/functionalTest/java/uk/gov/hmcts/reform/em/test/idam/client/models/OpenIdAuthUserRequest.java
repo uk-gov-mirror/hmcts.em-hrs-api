2fbfc6409866fd65218b4554214e4b58368925dc
mdayican <mdayican@gmail.com>
2024-05-29 08:49:27 +0100

    fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/em/test/idam/client/models/OpenIdAuthUserRequest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/test/idam/client/models/OpenIdAuthUserRequest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.em.test.idam.client.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
