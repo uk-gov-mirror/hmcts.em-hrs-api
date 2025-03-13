cb4e74e4007fdf89380c0550e2bb04e3e32475f3
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2025-03-13 16:13:23 +0000

    Em 6527 remove logs (#2370)

--- src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/MethodSecurityConfig.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/MethodSecurityConfig.java
@@ -1,12 +1,27 @@
 package uk.gov.hmcts.reform.em.hrs.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity()
 @Profile({"!integration-web-test"})
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+public class MethodSecurityConfig {
+    private final PermissionEvaluator permissionEvaluator;
+
+    public MethodSecurityConfig(PermissionEvaluator permissionEvaluator) {
+        this.permissionEvaluator = permissionEvaluator;
+    }
+
+    @Bean
+    public MethodSecurityExpressionHandler methodSecurityExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setPermissionEvaluator(permissionEvaluator);
+        return expressionHandler;
+    }
 }
