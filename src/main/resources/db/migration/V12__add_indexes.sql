5cc65ef6aed915865fdc9507ed78e2785da59edf
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2025-05-06 11:18:19 +0100

    Revert "Added Indexes (#2469)" (#2476)
    This reverts commit 30f8324fddd04feb81b72750550ee4833d24e52d.

--- src/main/resources/db/migration/V12__add_indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS hearing_recording_segment_hearing_recording_id ON hearing_recording_segment (hearing_recording_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS hearing_recording_sharee_hearing_recording_id ON hearing_recording_sharee (hearing_recording_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS audit_entry_hearing_recording_segment_id ON audit_entry (hearing_recording_segment_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS audit_entry_hearing_recording_sharee_id ON audit_entry (hearing_recording_sharee_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS audit_entry_hearing_recording_id ON audit_entry (hearing_recording_id);
