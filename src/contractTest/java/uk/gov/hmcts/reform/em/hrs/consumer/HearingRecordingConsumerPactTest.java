9399e77d53b21fe2e174664d69314f0c640ab644
mdayican <mdayican@gmail.com>
2025-06-27 07:53:58 +0100

    EM-6511 delete pact test

---
+++ src/contractTest/java/uk/gov/hmcts/reform/em/hrs/consumer/HearingRecordingConsumerPactTest.java
@@ -0,0 +1,65 @@
+package uk.gov.hmcts.reform.em.hrs.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactBuilder;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import lombok.extern.slf4j.Slf4j;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.hamcrest.Matchers.equalTo;
+
+@Slf4j
+@ExtendWith(PactConsumerTestExt.class)
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class HearingRecordingConsumerPactTest {
+
+    private static final String AUTH_TOKEN = "Bearer someAuthorizationToken";
+    private static final String SERVICE_AUTH_TOKEN = "Bearer someServiceAuthorizationToken";
+    private static final String POST_SEGMENTS_PATH = "/segments";
+
+    public Map<String, String> getHeaders() {
+        return Map.of(
+            AUTHORIZATION, AUTH_TOKEN,
+            "ServiceAuthorization", SERVICE_AUTH_TOKEN
+        );
+    }
+
+    @Pact(consumer = "em_hrs_hearing_recordings_api_consumer", provider = "em_hrs_hearing_recordings_api")
+    public V4Pact deleteHearingRecordingPact(PactBuilder builder) {
+        return builder
+            .usingLegacyDsl()
+            .given("Hearing recordings exist for given CCD case IDs to delete")
+            .uponReceiving("A request to delete hearing recordings for a case")
+            .path("/hearing-recordings")
+            .query("case_id=123456789")
+            .method("DELETE")
+            .headers(getHeaders())
+            .willRespondWith()
+            .status(HttpStatus.NO_CONTENT.value())
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "deleteHearingRecordingPact")
+    void testDeleteHearingRecordings(MockServer mockServer) {
+        SerenityRest
+            .given()
+            .headers(getHeaders())
+            .delete(mockServer.getUrl() + "/hearing-recordings?case_id=123456789")
+            .then()
+            .statusCode(HttpStatus.NO_CONTENT.value())
+            .body(equalTo(""));
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/reform/em/hrs/provider/HearingRecordingProviderTest.java
@@ -0,0 +1,92 @@
+package uk.gov.hmcts.reform.em.hrs.provider;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.em.hrs.controller.HearingRecordingController;
+import uk.gov.hmcts.reform.em.hrs.service.HearingRecordingService;
+import uk.gov.hmcts.reform.em.hrs.service.ScheduledTaskRunner;
+import uk.gov.hmcts.reform.em.hrs.service.SegmentDownloadService;
+import uk.gov.hmcts.reform.em.hrs.service.ShareAndNotifyService;
+import uk.gov.hmcts.reform.em.hrs.dto.HearingRecordingDto;
+
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ActiveProfiles("contract")
+@Provider("em_hrs_hearing_recording_api")
+//@PactBroker(
+//    scheme = "${PACT_BROKER_SCHEME:http}",
+//    host = "${PACT_BROKER_URL:localhost}",
+//    port = "${PACT_BROKER_PORT:80}",
+//    consumerVersionSelectors = {@VersionSelector(tag = "master")}
+//)
+@PactFolder("pacts")
+@IgnoreNoPactsToVerify
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(
+    value = { HearingRecordingController.class, ScheduledTaskRunner.class},
+    excludeAutoConfiguration = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class}
+)
+@AutoConfigureMockMvc(addFilters = false)
+public class HearingRecordingProviderTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private HearingRecordingService hearingRecordingService;
+
+    @MockitoBean
+    private ShareAndNotifyService shareAndNotifyService;
+
+    @MockitoBean
+    private SegmentDownloadService segmentDownloadService;
+
+    @MockitoBean(name = "ingestionQueue")
+    private LinkedBlockingQueue<HearingRecordingDto> ingestionQueue;
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        if (context != null) {
+            context.verifyInteraction();
+        }
+    }
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        if (context != null) {
+            context.setTarget(new MockMvcTestTarget(mockMvc));
+        }
+    }
+
+    @State("Hearing recordings exist for given CCD case IDs to delete")
+    public void setupHearingRecordingsExist() {
+        // Mock the service to do nothing on delete
+        doNothing().when(hearingRecordingService).deleteCaseHearingRecordings(List.of(123456789L, 987654321L));
+    }
+}
