9575c3a63c1fd3ce5c241ca1f2b81f020d480bbc
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2026-07-30 13:12:58 +0100

    Removed redundant files (#3140)

--- docker/dependencies/restart-local-environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-date
-COMPOSE_FILE="-f docker-compose-dependencies.yml"
-
-echo "compose file $COMPOSE_FILE"
-
-docker-compose ${COMPOSE_FILE} up -d --scale em-hrs-api=0
-docker-compose ${COMPOSE_FILE} logs -f
